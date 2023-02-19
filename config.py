2e30d214a27fa55df25e0cff05a494dcecb1472e
Stuart Mason <treeofthoughtblog@gmail.com>
2023-02-19 11:52:10 -0500

    fix config

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SQLALCHEMY_DATABASE_URI = "postgresql://jlppbc-admin:@localhost:5432/jlppapi"
-  SQLALCHEMY_ECHO = True
 
   @staticmethod
   def init_app(app):
@@ -15,8 +14,9 @@ class Config:
 
 class HerokuConfig(Config):
   heroku_db_url = os.environ.get('DATABASE_URL')
-  sqlalchemy_support = heroku_db_url.replace('postgres://', 'postgresql://')
-  SQLALCHEMY_DATABASE_URI = sqlalchemy_support
+  if heroku_db_url:
+    sqlalchemy_support = heroku_db_url.replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = sqlalchemy_support
 
 config = {
     'default': Config,
